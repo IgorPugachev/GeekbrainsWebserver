5a32259e9e61ae8deebe59db6fa8b28a0d239882
IgorPugachev <igor0514@gmail.com>
2021-02-19 09:38:23 +1100

    Corrections of file script.js.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
 
     res.end(body);
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port);
 
